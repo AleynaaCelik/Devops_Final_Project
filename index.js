46877aee1a9bc04b0dafdcda01763a10d4420a72
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-05-14 23:12:27 +0300

    changes have been made

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const redis = require('redis');
 const connectRedis = require('connect-redis');
 const bodyParser = require('body-parser');
 const mysql = require("mysql2");
-const { v4: uuidv4 } = require('uuid'); // Benzersiz kimlik oluşturmak için
-const amqp = require('amqplib/callback_api'); //rabbitmq için
-const mongoose = require('mongoose');//mongodb için
+const { v4: uuidv4 } = require('uuid');
+const amqp = require('amqplib/callback_api');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(bodyParser.json());
@@ -22,10 +22,10 @@ const connection = mysql.createPool({
     password: "root",
     database: "records"
 });
-  
+
 connection.getConnection((err, connection) => {
     if (err) {
-        console.error('MySQL bağlanti hatasi: ' + err);
+        console.error('MySQL bağlantı hatası: ' + err);
     } else {
         console.log('MySQL bağlantısı başarılı.');
     }
@@ -36,10 +36,11 @@ const redisClient = redis.createClient({
     port: 6379
 });
 
-redisClient.on('error', function (err) {
+redisClient.on('error', (err) => {
     console.log('Could not establish a connection with redis. ' + err);
 });
-redisClient.on('connect', function (err) {
+
+redisClient.on('connect', () => {
     console.log('Connected to redis successfully');
 });
 
@@ -49,150 +50,128 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: false, 
-        httpOnly: false, 
-        maxAge: 1000 * 60 * 10 
+        secure: false,
+        httpOnly: false,
+        maxAge: 1000 * 60 * 10
     }
 }));
 
-// Benzersiz kullanıcı kimliği oluşturmak için fonksiyon
 function generateUniqueId() {
-    return uuidv4(); // UUID kullanarak benzersiz bir kimlik oluştur
+    return uuidv4();
 }
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.send("hello world");
 });
 
-app.get("/profile", (req,res)=>{
+app.get("/profile", (req, res) => {
     const session = req.session;
-    if(session.username) {
+    if (session.username) {
         res.send("Welcome");
-        return;
+    } else {
+        res.sendStatus(401);
     }
-    res.sendStatus(401);
 });
-
 
 app.post("/register", (req, res) => {
     const { username, password } = req.body;
-    const userId = generateUniqueId(); // Benzersiz bir ID oluşturulur
-    connection.query('INSERT INTO users (id, username, password) VALUES (?, ?, ?)', [userId, username, password], (error, results, fields) => {
+    const userId = generateUniqueId();
+    connection.query('INSERT INTO users (id, username, password) VALUES (?, ?, ?)', [userId, username, password], (error) => {
         if (error) {
             console.error('MySQL query error: ', error);
             res.status(500).send("An error occurred while processing your request");
-            return;
+        } else {
+            res.status(201).send("User registered successfully");
         }
-        res.status(201).send("User registered successfully");
     });
 });
-
-
 
 app.post("/login", (req, res) => {
     const session = req.session;
     const { username, password } = req.body;
-    connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (error, results, fields) => {
+    connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (error, results) => {
         if (error) {
             console.error('MySQL query error: ', error);
             res.send("An error occurred while processing your request");
-            return;
-        }
-        if (results.length > 0) {
-            const userId = results[0].id; // Kullanıcının ID'si alınır
-            session.userId = userId; // Oturum verisine kullanıcı ID'si eklenir
+        } else if (results.length > 0) {
+            const userId = results[0].id;
+            session.userId = userId;
             res.send("Login successful");
         } else {
-            // Kayıtlı kullanıcı bulunamadı, kullanıcıyı kayıt olma sayfasına yönlendir
             res.redirect("/register");
         }
     });
 });
 
-
-
-app.get("/logout",(req,res)=>{
-    req.session.destroy((err)=>{
-        if(err) {
+app.get("/logout", (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
             console.error('Session destroy error: ', err);
             res.send("An error occurred while processing your request");
-            return;
+        } else {
+            res.send("Logout successful");
         }
-        res.send("Logout successful");
     });
 });
 
-
-// Middleware oluşturma
 function checkLoggedIn(req, res, next) {
-    // Oturum bilgilerini kontrol et
     if (req.session.userId) {
-        // Kullanıcı giriş yapmışsa, bir sonraki middleware'e geç
         next();
     } else {
-        // Kullanıcı giriş yapmamışsa, erişimi reddet
         res.status(401).send("Unauthorized");
     }
-} 
+}
 
-
-//Ürünler İçin EndPointler
-
-// Ürünleri listeleme endpoint'i
 app.get("/products", checkLoggedIn, (req, res) => {
-    const userId = req.session.userId; // Oturumda kayıtlı kullanıcı kimliği
-    connection.query('SELECT * FROM products WHERE user_id = ?', [userId], (error, results, fields) => {
+    const userId = req.session.userId;
+    connection.query('SELECT * FROM products WHERE user_id = ?', [userId], (error, results) => {
         if (error) {
             console.error('MySQL query error: ', error);
             res.status(500).send("An error occurred while processing your request");
-            return;
+        } else {
+            res.json(results);
         }
-        res.json(results);
     });
 });
-// Yeni bir ürün eklemek için POST endpoint'i
+
 app.post("/products", checkLoggedIn, (req, res) => {
     const { name, price, description } = req.body;
-    const userId = req.session.userId; // Oturumda kayıtlı kullanıcı kimliği
-    connection.query('INSERT INTO products (name, price, description, user_id) VALUES (?, ?, ?, ?)', [name, price, description, userId], (error, results, fields) => {
+    const userId = req.session.userId;
+    connection.query('INSERT INTO products (name, price, description, user_id) VALUES (?, ?, ?, ?)', [name, price, description, userId], (error) => {
         if (error) {
             console.error('MySQL query error: ', error);
             res.status(500).send("An error occurred while processing your request");
-            return;
+        } else {
+            res.status(201).send("Product added successfully");
         }
-        res.status(201).send("Product added successfully");
     });
 });
 
-// Belirli bir ürünü güncellemek için PUT endpoint'i
 app.put("/products/:id", checkLoggedIn, (req, res) => {
     const productId = req.params.id;
     const { name, price, description } = req.body;
-    connection.query('UPDATE products SET name = ?, price = ?, description = ? WHERE id = ?', [name, price, description, productId], (error, results, fields) => {
+    connection.query('UPDATE products SET name = ?, price = ?, description = ? WHERE id = ?', [name, price, description, productId], (error) => {
         if (error) {
             console.error('MySQL query error: ', error);
             res.status(500).send("An error occurred while processing your request");
-            return;
+        } else {
+            res.send("Product updated successfully");
         }
-        res.send("Product updated successfully");
     });
 });
 
-// Belirli bir ürünü silmek için DELETE endpoint'i
 app.delete("/products/:id", checkLoggedIn, (req, res) => {
     const productId = req.params.id;
-    connection.query('DELETE FROM products WHERE id = ?', [productId], (error, results, fields) => {
+    connection.query('DELETE FROM products WHERE id = ?', [productId], (error) => {
         if (error) {
             console.error('MySQL query error: ', error);
             res.status(500).send("An error occurred while processing your request");
-            return;
+        } else {
+            res.send("Product deleted successfully");
         }
-        res.send("Product deleted successfully");
     });
 });
 
-
-// RabbitMQ connection
 let channel = null;
 amqp.connect('amqp://rabbitmq', (err, conn) => {
     if (err) {
@@ -204,28 +183,23 @@ amqp.connect('amqp://rabbitmq', (err, conn) => {
             } else {
                 channel = ch;
                 console.log('Connected to RabbitMQ successfully');
-                
-                // RabbitMQ bağlantısı başarıyla kurulduktan sonra order işlemlerini gerçekleştir
-                // order creation
+
                 app.post("/orders", (req, res) => {
                     const { items, user_id, total } = req.body;
-
-                    // Insert order into MySQL
-                    connection.query('INSERT INTO orders (items, user_id, total) VALUES (?, ?, ?)', [items, user_id, total], (error, results, fields) => {
+                    connection.query('INSERT INTO orders (items, user_id, total) VALUES (?, ?, ?)', [items, user_id, total], (error) => {
                         if (error) {
                             console.error('MySQL query error: ', error);
                             res.status(500).send("An error occurred while processing your request");
-                            return;
+                        } else {
+                            const order = { items, user_id, total };
+                            channel.assertQueue('order_queue', { durable: true });
+                            channel.sendToQueue('order_queue', Buffer.from(JSON.stringify(order)), { persistent: true });
+                            res.status(201).send("Order created successfully");
                         }
-
-                        // Publish order to RabbitMQ
-                        const order = { items, user_id, total };
-                        channel.assertQueue('order_queue', { durable: true });
-                        channel.sendToQueue('order_queue', Buffer.from(JSON.stringify(order)), { persistent: true });
-
-                        res.status(201).send("Order created successfully");
                     });
                 });
+
+                listenQueue(channel);
             }
         });
     }
@@ -238,9 +212,7 @@ const listenQueue = async (channel) => {
         console.log(`Worker is listening to the queue: ${queue}`);
         channel.consume(queue, (msg) => {
             if (msg !== null) {
-                // Gelen mesajı işle
                 console.log('Received message:', msg.content.toString());
-                // Mesajı işlediğinizde kuyruktan kaldırın
                 channel.ack(msg);
             }
         });
@@ -250,42 +222,8 @@ const listenQueue = async (channel) => {
     }
 };
 
-// Kuyruğu dinlemeye başla
-amqp.connect('amqp://rabbitmq', (err, conn) => {
-    if (err) {
-        console.error('RabbitMQ connection error: ', err);
-    } else {
-        conn.createChannel((err, ch) => {
-            if (err) {
-                console.error('RabbitMQ channel creation error: ', err);
-            } else {
-                channel = ch;
-                console.log('Connected to RabbitMQ successfully');
-
-                // Kuyruğu dinlemeye başla
-                listenQueue(channel);
-            }
-        });
-    }
-});
-const consumeOrderQueue = () => {
-    channel.consume('order_queue', async (msg) => {
-        const order = JSON.parse(msg.content.toString());
-        console.log('Received order:', order);
-
-        // Siparişteki her bir ürün için stoktan düşüş işlemini gerçekleştir
-        for (const item of order.items) {
-            await decreaseProductStock(item.productId, item.quantity);
-        }
-
-        // Siparişin işlendiğini doğrulamak için RabbitMQ'ya ACK gönder
-        channel.ack(msg);
-    });
-};
-//Stoktan ürün düşüşü İşlemi
 const decreaseProductStock = async (productId, quantity) => {
     try {
-        // Ürün stok miktarını azalt
         await connection.query('UPDATE products SET stock = stock - ? WHERE id = ?', [quantity, productId]);
         console.log(`Stock decreased for product ${productId} by ${quantity}`);
     } catch (error) {
@@ -293,56 +231,70 @@ const decreaseProductStock = async (productId, quantity) => {
         throw error;
     }
 };
-// Ürün yorumları kısmı 
 
+const consumeOrderQueue = () => {
+    channel.consume('order_queue', async (msg) => {
+        const order = JSON.parse(msg.content.toString());
+        console.log('Received order:', order);
 
-// MongoDB bağlantısı
-mongoose.connect('mongodb://mongo:27017/my_database', { useNewUrlParser: true, useUnifiedTopology: true })
+        for (const item of order.items) {
+            await decreaseProductStock(item.productId, item.quantity);
+        }
+
+        channel.ack(msg);
+    });
+};
+
+mongoose.connect('mongodb://root:password@mongo:27017/my_database?authSource=admin')
   .then(() => console.log('MongoDB bağlantısı başarılı'))
   .catch((err) => console.error('MongoDB bağlantı hatası:', err));
 
-// MongoDB Schema (Model) tanımı
 const CommentSchema = new mongoose.Schema({
-  orderId: String,
-  userId: String,
-  content: String,
-  rating: Number
+    orderId: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    }
 });
 
-// MongoDB Modeli
 const Comment = mongoose.model('Comment', CommentSchema);
 
-// Middleware - JSON verilerini işleme
 app.use(express.json());
 
-// Yorum ekleme endpoint'i
 app.post('/comments', async (req, res) => {
-  const { orderId, userId, content, rating } = req.body;
-  
-  try {
-    // Yeni yorum oluştur
-    const comment = new Comment({ orderId, userId, content, rating });
-    await comment.save();
-    res.status(201).send('Yorum başarıyla eklendi');
-  } catch (err) {
-    console.error('Yorum ekleme hatası:', err);
-    res.status(500).send('Yorum eklenirken bir hata oluştu');
-  }
+    const { orderId, userId, content, rating } = req.body;
+
+    try {
+        const comment = new Comment({ orderId, userId, content, rating });
+        await comment.save();
+        res.status(201).send('Yorum başarıyla eklendi');
+    } catch (err) {
+        console.error('Yorum ekleme hatası:', err);
+        res.status(500).send('Yorum eklenirken bir hata oluştu');
+    }
 });
 
-// Tüm yorumları getiren endpoint
 app.get('/comments', async (req, res) => {
-  try {
-    const comments = await Comment.find();
-    res.json(comments);
-  } catch (err) {
-    console.error('Yorumlar getirme hatası:', err);
-    res.status(500).send('Yorumlar getirilirken bir hata oluştu');
-  }
+    try {
+        const comments = await Comment.find();
+        res.json(comments);
+    } catch (err) {
+        console.error('Yorumlar getirme hatası:', err);
+        res.status(500).send('Yorumlar getirilirken bir hata oluştu');
+    }
 });
 
-
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("server is running on port 3000");
 });
